03c0fe287d61d02d6a2b896b7250578d3ab3b2f8
unknown <skd.itkn@gmail.com>
2023-08-14 12:14:55 -0700

    added basic structure

---
+++ fridgefy/src/Components/RecipesPage.jsx
@@ -0,0 +1,10 @@
+import RecipesInfoDiv from "./RecipesInfo.jsx"
+
+export default function RecipesPageSection(){
+    return (
+        <section>
+            RecipesPage
+            <RecipesInfoDiv/>
+        </section>
+    )
+}
