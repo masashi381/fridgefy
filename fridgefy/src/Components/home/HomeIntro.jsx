c30347f1b943c7ca4844f7f699f1718c053d8c21
masashi381 <121969638+masashi381@users.noreply.github.com>
2023-08-16 10:19:58 -0700

    Home page is temporary done

--- fridgefy/src/Components/home/HomeIntro.jsx
+++ fridgefy/src/Components/home/HomeIntro.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
 
 export default function HomeIntro() {
   return (
-    <div>
-      <p>Welcome to [Your Recipe Search Site] - your ultimate destination for culinary inspiration!
+    <Div>
+      <P>Welcome to [Your Recipe Search Site] - your ultimate destination for culinary inspiration!
         Whether you're a seasoned chef or a novice in the kitchen, we're here to make your cooking journey delightful and flavorful.
         Our extensive collection of recipes caters to all tastes and dietary preferences, ensuring that you'll find something to excite your taste buds. 
         From quick and easy weeknight dinners to show-stopping desserts, we've got you covered. 
         Join us in exploring the art of cooking as we embark on a flavorful adventure together!
-      </p>
-      <h2>Please log in to start!</h2>
-    </div>
+      </P>
+      <H2>Please log in to start!</H2>
+    </Div>
   )
-}
+};
+
+const Div = styled.div`
+  width: 50vw;
+`;
+
+const P = styled.p`
+  width: 80%;
+  margin: 0 auto;
+`;
+
+const H2 = styled.h2`
+  width: 80%;
+  margin: 1opx auto;
+  text-align: center;
+`
